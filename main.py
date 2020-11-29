b3fa25f5afcb7b26fb0c4d276578a1577d05d3a6
ZetroR <75221145+ZetroR@users.noreply.github.com>
2020-11-29 21:40:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def main():
-    # Use a breakpoint in the code line below to debug your script.
-    print('My first git program')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
